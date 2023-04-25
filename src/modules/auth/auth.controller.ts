2c2911e333c34907a56d00ec89bc95ba1c827bab
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-04-25 22:58:13 +0100

    chore: Remove auth entity

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, UseGuards, HttpCode, Request } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  HttpStatus,
+  UseGuards,
+  HttpCode,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { sendSuccessResponse } from "../../core/responses/success.responses";
-import { LocalAuthGuard } from "./guards/local-auth.guard";
+import { sendSuccessResponse } from '../../core/responses/success.responses';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
